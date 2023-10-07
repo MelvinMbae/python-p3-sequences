120590b18a1a308b767e58e1cdc062d863e41efa
MelvinMbae <melvinmuthomi95@gmail.com>
2023-10-07 14:27:21 +0300

    first attempt

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+
+    n1, n2 = 0,1
+    count = 0
+    
+    
+    while count < length:
+        print(n1)
+        nth = n1+n2
+        n1 = n2
+        n2 = nth
+        count +=1
+        
+print(print_fibonacci(10))
